1e8f9f842b503b52cd9def1a866809eadf3940cc
shalakasankar <shalakasankar@gmail.com>
2021-11-25 22:04:47 +0530

    createEdge function and getters and setters for data

--- Graph.py
+++ Graph.py
@@ -1,41 +1,98 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Nov 20 12:21:46 2021
 
-@author: shala
-"""
-import Node
+from Node import Node
 import numpy as np
 class Graph:
     
     def __init__(self,n,matrix):
-        node_array = list()
+        self.node_array = list()
        
         count=1
         pos=1
         
+        self.block1=[1,2,3,10,11,12,19,20,21]
+        self.block2=[4,5,6,13,14,15,22,23,24]
+        self.block3=[7,8,9,16,17,18,25,26,27]
+        self.block4=[28,29,30,37,38,39,46,47,48]
+        self.block5=[31,32,33,40,41,42,49,50,51]
+        self.block6=[34,35,36,43,44,45,52,53,54]
+        self.block7=[55,56,57,64,65,66,73,74,75]
+        self.block8=[58,59,60,67,68,69,76,77,78]
+        self.block9=[61,62,63,70,71,72,79,80,81]
+
         grid=np.zeros((n,n))
         for i in range(n):
             for j in range(n):
-                grid[i][j]=pos;
+                grid[i][j]=pos
                 pos+=1
 
         for i in range(n):
            for j in range(n):
               n=Node(count,matrix[i][j])
-              node_array.append(n)
+              self.node_array.append(n)
               count=+1
               
-    def displayGraph(n,node_array):
-        for i in node_array:
+    def displayGraph(self,n):
+        for i in range (0,len(self.node_array)):
             if i%n==0:
-                 print("\n")
+                print("\n")
             else:
-                print(node_array[i].printNode())
+                print(self.node_array[i].printNode())
+
+    def updateData(self,id,data):
+        for i in range(len(self.node_array)):
+            if self.node_array[i].id==id:
+                self.node_array[i].setData(data)
+    
+    def createEdge(self,order,grid):         #put in graph class
+        for i in range(order):
+            for j in range(order):
+                key=grid[i][j]
+                rows=list()
+                for k in range(0,order):
+                    value=grid[i][k]
+                    if value!=key:
+                     rows.append(value)
+
+                cols =list()
+                for k in range(0,order):
+                    value=grid[k][j]
+                    if value!=key:
+                        cols.append(value)
+
+
+                        block=list()
+                if key in self.block1:
+                    block=self.block1
+                elif key in self.block2:
+                    block=self.block2
+                elif key in self.block3:
+                    block=self.block3
+                elif key in self.block4:
+                    block=self.block4
+                elif key in self.block5:
+                    block=self.block5
+                elif key in self.block6:
+                    block=self.block6
+                elif key in self.block7:
+                    block=self.block7
+                elif key in self.block8:
+                    block=self.block8
+                elif key in self.block9:
+                    block=self.block9
+
+                
+                self.node_array[key].updateDict(key,rows,cols,block)             
+                    
+                
+                
+    
+
+
+
             
-    def createEdge(grid):
+   
                             
-         
+     
             
          
             
--- Node.py
+++ Node.py
@@ -1,26 +1,32 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Nov 20 12:19:52 2021
-
-@author: shala
-"""
 
 class Node:
     def __init__(self, idx, data) : # Constructor   
-        """
-        id : Integer (1, 2, 3, ...)
-        """
+        
         self.id = idx
         self.data = data
         self.connectedTo = dict()
+        
 
-
-  #  def createEdge(n,node):
-      #  for i in range(n**2):
+  
           
     def printNode(self):
         print(self.data)
         
-           
-       
-         
+#function to access and change data using id-done
+    def setData(self,data):
+        self.data=data
+
+    def getData(self,data):
+        return self.data
+
+    
+    def updateDict(self,key,rows,col,block):
+        value=list()
+        value.append(rows)
+        value.append(col)
+        value.append(block)
+        d={key: value}
+        self.connectedTo.update(d)
+
+   
+            
